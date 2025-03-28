17c8045a1621d9e5baaccdc9f79e15550a8dd6a8
Влад Крахмалев <vlad.krakhmalev.web@gmail.com>
2025-03-28 08:44:22 +0300

    feat: add spiner

---
+++ src/shared/ui/spiner/Spiner.tsx
@@ -0,0 +1,20 @@
+import { FC, useMemo } from 'react';
+import './Spiner.css'
+import clsx from 'clsx';
+
+interface IProps {
+  size?: number
+  contrast?: boolean
+  className?: string
+}
+
+export const Spiner: FC<IProps> = ({ size = 30, contrast, className }) => {
+
+  const style = useMemo(() => {
+    return {width: `${size}px`, height: `${size}px`, borderWidth: `${size / 8}px`}
+  }, [size])
+
+  return (
+    <i className={clsx('spiner', contrast && '_contrast', className)} style={style} />
+  );
+};
