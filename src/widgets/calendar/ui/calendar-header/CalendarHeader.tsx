04038facefd4c30217bf354feb84f7ff65770c89
Влад Крахмалев <vlad.krakhmalev.web@gmail.com>
2025-04-22 12:50:34 +0300

    feat(calendar): add view switcher, add theme switcher, add title

--- src/widgets/calendar/ui/calendar-header/CalendarHeader.tsx
+++ src/widgets/calendar/ui/calendar-header/CalendarHeader.tsx
@@ -1,25 +1,46 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import './CalendarHeader.css'
 import { useCalendarStore } from '../../model/calendarStore';
 import { Button } from '@shared/ui/button';
-import { AddTaskButton } from '@features/add-task';
+import { Switcher } from '@shared/ui/switcher';
+import { useTheme } from '@shared/lib/theme';
+import { Modal } from '@shared/ui/modal';
 
 export const CalendarHeader: FC = () => {
-  const { prevWeek, nextWeek, resetWeek } = useCalendarStore()
+  const { startDate, prevWeek, nextWeek, resetWeek } = useCalendarStore()
+  const { theme, toggleTheme } = useTheme()
+  const month = startDate.format("MMMM YYYY")
+  const [isOpen, setIsOpen] = useState<boolean>(false)
 
   return (
     <div className="calendar-header">
-      <AddTaskButton/>
+      <Button iconBefore='sidebar' onClick={() => setIsOpen(true)}/>
 
-      <Button iconBefore="calendar-day" onClick={resetWeek}>
-        Сегодня
-      </Button>
+      <Modal isOpen={isOpen} position='left' onClose={() => setIsOpen(false)}>
+        <Button
+          iconBefore={theme === 'light' ? 'moon' : 'brightness'}
+          onClick={toggleTheme}
+        />
+      </Modal>
+
+      <p className="calendar-header__title">{month}</p>
 
       <div className="calendar-header__arrows">
         <Button iconBefore="angle-left" onClick={prevWeek}/>
         <Button iconBefore="angle-right" onClick={nextWeek}/>
       </div>
-      
+
+      <Button iconBefore="calendar-day" onClick={resetWeek}>
+        Сегодня
+      </Button>
+
+      <Switcher
+        defaultValue='week'
+        options={[
+          { value: 'week', title: 'Неделя' },
+          { value: 'month', title: 'Месяц' },
+        ]}
+      />
     </div>
   );
 };
